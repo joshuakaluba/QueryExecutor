ad7aaecf0eb74c9dd4123a88b8ec8c771fac63e2
Joshua Kaluba <kalubaj@icloud.com>
2016-04-30 11:11:22 -0400

    Create methods for Creating, Updating and Deleting student records

--- QueryExecutor/Program.cs
+++ QueryExecutor/Program.cs
@@ -22,46 +22,95 @@ namespace QueryExecutor
             object StudentCount = GetNumberOfStudents();
             Console.WriteLine("Total number of students : " + StudentCount.ToString());
 
+            CreateStudent();
+            UpdateStudent();
+            DeleteStudent();
+
 
             Console.Read();
         }
 
-        public static object GetNumberOfStudents()
+
+        private static void CreateStudent()
         {
-            //Get db connection string
+            //Get db connection string.
             DBMSSQL Db = GetMSSQLDB();
 
-            //Declare the scalar query
+            //Declare the insert statement.
+            String Query = "INSERT INTO Student (LastName, FirstName, StreetAddress) Values ('Murkel','Cliff','115 Niagara Ave');";
+
+            //Execute the query
+            Db.NonQuery(Query);
+
+        }
+
+        private static void UpdateStudent()
+        {
+            //Get db connection string.
+            DBMSSQL Db = GetMSSQLDB();
+
+            //Declare the update statement.
+            String Query = "UPDATE Student SET LastName = 'Peters' WHERE ID='4';";
+
+            //Execute the query
+            Db.NonQuery(Query);
+        }
+
+        private static void DeleteStudent()
+        {
+            //Get db connection string.
+            DBMSSQL Db = GetMSSQLDB();
+
+            //Declare the delete statement.
+            String Query = "DELETE From Student WHERE ID='4';";
+
+            //Execute the query
+            Db.NonQuery(Query);
+
+        }
+
+
+
+        public static object GetNumberOfStudents()
+        {
+            //Get db connection string.
+            DBMSSQL Db = GetMSSQLDB();
+
+            //Declare the scalar query.
             String Query = "Select COUNT(*) FROM Student;";
 
-
+            //Pass the query to the scalar db method and execute the query.
             object StudentCount = Db.Scalar(Query);
+
+            //return results.
             return StudentCount;
         }
 
         public static List<Student> GetStudentInfo()
         {
-            //Get db connection string
+            //Returns a list of student records.
+
+            //Get db connection string.
             DBMSSQL Db = GetMSSQLDB();
 
-            //Declare the query
+            //Declare the query.
             String Query = "SELECT LastName, FirstName, StreetAddress from Student;";
 
-            //Execute the query and store the query results from the database in a list
+            //Execute the query and store the query results from the database in a list.
             List<Student> Students = new List<Student>(Db.Query<Student>(Query));
 
-            //return results
+            //return results.
             return Students;
             
         }
 
         private static DBMSSQL GetMSSQLDB()
         {
-            //Private method to hold connection string info for the database
+            //Private method to hold connection string info for the database.
 
             //Port is an optional parameter in the DB config and is not needed for MSSQL databases.
-            DBMSSQL Db = new DBMSSQL(new DbConfig("adminkaluba", "joshkal", "Guyk433%", "198.71.225.145"));
-            //DBMSSQL Db = new DBMSSQL(new DbConfig("dbName", "user", "password", "host"));
+            DBMSSQL Db = new DBMSSQL(new DbConfig("dbName", "user", "password", "host"));
+
             return Db;
         }
     }
